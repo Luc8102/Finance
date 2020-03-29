016ee135ec43b6a78b65959b978963203c45fc1e
Mihir <55668536+mp8dy@users.noreply.github.com>
2020-03-29 12:36:14 -0400

    File implementation

--- finance.java
+++ finance.java
@@ -2,24 +2,20 @@ package Finance;
 
 
 import java.util.Scanner;
+import java.io.*;
 
-public class finance
+public class Finance
 {
-  public void qna()
-  {
-    float total=0;
-    float weekly=0;
-    Scanner scan = new Scanner(System.in);
-    System.out.println("What is your expected annual expendenture for the years after retirement?");
-    float retirementSpending = scan.nextFloat();
-    System.out.println("How much do you intend on spending each year before retirement?");
-    float expenses = scan.nextFloat();
-    System.out.println("How much have you saved for retirement?");
-    float retirementSavings = scan.nextFloat();
-    System.out.println("For tax purposes, what is your yearly income?");
-    float income = scan.nextFloat();
-    if(income<9700)
+    private float total;
+    private float weekly;
+    private float retirementSpending;
+    private float expenses;
+    private float retirementSavings;
+    private float income;
+    
+    public Finance()
     {
+<<<<<<< Updated upstream
       income-=(income*0.1);
     }
     else if(income<39475)
@@ -81,4 +77,129 @@ public class finance
       return false;
     }
   }
+=======
+      total = 0;
+      weekly = 0;
+      Scanner scan = new Scanner(System.in);
+      System.out.println("What is your expected annual expendenture for the years after retirement?");
+      retirementSpending = getValue(scan.nextLine());
+      writer("retirementSpending: " + retirementSpending);
+      System.out.println("How much do you intend on spending each year before retirement?");
+      expenses = getValue(scan.nextLine());
+      writer("expenses: " + expenses);
+      System.out.println("How much have you saved for retirement?");
+      retirementSavings = getValue(scan.nextLine());
+      writer("retirementSavings: " + retirementSavings);
+      System.out.println("For tax purposes, what is your yearly income?");
+      income = getValue(scan.nextLine());
+      writer("income: " + income);
+    } //ends default constructor
+    
+//     public Finance(String filename)
+//     {
+//         Scanner input = new Scanner(new File(filename));
+//         while(input.hasNext())
+//         {
+//             
+//         } //ends while
+//     } //ends 1-arg constructor
+    
+   public void qna()
+   {
+//       float total = 0;
+//       float weekly = 0;
+//       Scanner scan = new Scanner(System.in);
+//       System.out.println("What is your expected annual expendenture for the years after retirement?");
+//       float retirementSpending = scan.nextFloat();
+//       System.out.println("How much do you intend on spending each year before retirement?");
+//       float expenses = scan.nextFloat();
+//       System.out.println("How much have you saved for retirement?");
+//       float retirementSavings = scan.nextFloat();
+//       System.out.println("For tax purposes, what is your yearly income?");
+//       float income = scan.nextFloat();
+      if(income<9700)
+      {
+         income-=(income*0.1);
+      }
+      else if(income<39475)
+      {
+         income-=(income*0.12);
+      }
+      else if(income<84200)
+      {
+         income-=(income*0.22);
+      }
+      else if(income<160725)
+      {
+         income-=(income*0.24);
+      }
+      else if(income<204100)
+      {
+         income-=(income*0.32);
+      }
+      else if(income<510300)
+      {
+         income-=(income*0.35);
+      }
+      else
+      {
+         income-=(income*0.37);
+      }
+      System.out.println("What is your age?");
+      byte age = scan.nextByte();
+   //Everything before this line are preliminary questions to ask the user in order to calculate everything else.
+   //below is a loop that was initally supposed to take the spending of every day and make a weekly report, but this may be misplaced
+      System.out.println("You'll have $" + retirement(retirementSpending,income,retirementSavings,age,expenses)+ " years of savings at $" + retirementSpending+ " per year if you retire at 67!");
+   } //ends qna
+   
+   //below is the calculation to find how much money the user will have at age 65
+   public float retirement(float retirementSpending, float income, float retirementSavings, byte age, float expenses)
+   {
+      int yearsLeft=67-age;
+      float moneyAt67=retirementSavings+(yearsLeft*(income-expenses));
+      float youHave_YearsAfterRetirement=moneyAt67/retirementSpending;
+      return youHave_YearsAfterRetirement;
+   }
+   public boolean goalmet(float weeklyspending, float expenses)
+   {
+      if(weeklyspending<=expenses/52)
+      {
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
+   
+   public void writer(String s)
+   {
+      try
+      {
+         BufferedWriter writer = new BufferedWriter(new FileWriter("store.txt", true));
+         writer.append(s + "\n");
+        
+         writer.close();
+      } //ends try
+      catch(IOException ex)
+      {
+         System.out.println("Fail");
+      } //ends catch
+   } //ends writer
+   
+   public float getValue(String s)
+   {
+        char[] charray = s.toCharArray();
+        String store = "";
+        
+        for(int i = 0; i < charray.length; i++)
+        {
+            char c = charray[i];
+            if(Character.isDigit(c))
+                store += c;
+        } //ends for
+        
+        return(Float.parseFloat(store));
+   } //ends getValue
+>>>>>>> Stashed changes
 }
