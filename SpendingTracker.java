8f551e7ba804d6e4f28202d14a7a184087c118e7
Gloria Sun <gloriasun4@gmail.com>
2020-03-29 11:30:30 -0400

    added transaction arraylist

--- SpendingTracker.java
+++ SpendingTracker.java
@@ -5,18 +5,25 @@ public class SpendingTracker
     private String phoneNumber;
     private String category;
     private ArrayList<String> categories;
+    private ArrayList<double[]> transactions;
     
     public static void main(String[] args) { //to test values
     }
     public SpendingTracker(String n) { //setter
       phoneNumber = n;
       categories = new ArrayList<String>(); // or we can do preset
+      transactions = new ArrayList<double[]>();
     }
     public void addCategories(String c) {
       categories.add(c);
+      //transactions.add(double[]);
+    }
+    public void makeTransaction(String t) {
+      //t.getValue();
     }
     public double getValue(String s) {
       int i = s.indexOf(" ");
+      category = s.substring(i);
       value = Double.parseDouble(s.substring(0,i));
       return value;
     } //ends getValue
