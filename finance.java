f2adba0ab7c6254bcb89bee374b1b290a22fac88
Luc Barenghein <luc.coins@gmail.com>
2020-03-29 18:40:31 -0400

    Add files via upload

--- finance.java
+++ finance.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class finance
 {
-  public void qna()
+  public static void main (String[] args)
   {
     float total=0;
     float weekly=0;
     Scanner scan = new Scanner(System.in);
-    System.out.println("What is your expected annual expendenture for the years after retirement?");
+    System.out.println("What is your expected annual spending for each year after retirement?");
     float retirementSpending = scan.nextFloat();
     System.out.println("How much do you intend on spending each year before retirement?");
     float expenses = scan.nextFloat();
@@ -44,20 +44,27 @@ public class finance
       income-=(income*0.37);
     }
     System.out.println("What is your age?");
-    byte age = scan.nextByte();
+    float age = scan.nextFloat();
  //Everything before this line are preliminary questions to ask the user in order to calculate everything else.
  //below is a loop that was initally supposed to take the spending of every day and make a weekly report, but this may be misplaced
-    System.out.println("You'll have $" +retirement(retirementSpending,income,retirementSavings,age,expenses)+ " years of savings at $"+retirementSpending+"per year of you retire at 67!");
+    if (retirement(retirementSpending,income,retirementSavings,age,expenses)<=0)
+    {
+      System.out.println("At this rate, you will not have enough money to retire.");
+    }
+    else
+    {
+      System.out.println("You'll have " +retirement(retirementSpending,income,retirementSavings,age,expenses)+ " years of savings at $"+retirementSpending+"per year if you retire at 67!");
+    }
   }
   //below is the calculation to find how much money the user will have at age 65
-  public float retirement(float retirementSpending, float income, float retirementSavings, byte age, float expenses)
+  public static float retirement(float retirementSpending, float income, float retirementSavings, float age, float expenses)
   {
-    byte yearsLeft=67-age;
+    float yearsLeft=67-age;
     float moneyAt67=retirementSavings+(yearsLeft*(income-expenses));
     float youHave_YearsAfterRetirement=moneyAt67/retirementSpending;
-    return youHave_YearsAfterRetirement67;
+    return youHave_YearsAfterRetirement;
   }
-  public boolean goalmet(weeklyspending, expenses)
+  public boolean goalmet(float weeklyspending, float expenses)
   {
     if(weeklyspending<=expenses/52)
     {
